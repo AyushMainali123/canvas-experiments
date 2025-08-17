9178f8d3d611a8a6afd3b0db9d161af01f61a502
AyushMainali123 <ayushmainali11@gmail.com>
2025-08-17 17:21:31 +0545

    chore: initialized space invader

--- sine-wave/index.js
+++ sine-wave/index.js
@@ -24,11 +24,11 @@ function animate() {
 
     for(let i = 0; i < canvas.width; i++) {
         let height = canvas.height / 2;
-        ctx.lineTo(i, height +  AMPLITUDE * Math.sin(degreeToRadian(increment)) *  Math.sin(degreeToRadian(i * FREQUENCY + increment)));
+        ctx.lineTo(i, height +  AMPLITUDE * Math.cos(degreeToRadian(increment)) *  Math.cos(degreeToRadian(i * FREQUENCY + increment)));
     }
 
     increment += FREQUENCY;
-    ctx.strokeStyle = `hsl(0, ${Math.min(Math.abs(Math.sin(degreeToRadian(increment / 2)) * 100), 100)}%, 50%)`;
+    ctx.strokeStyle = `hsl(0, ${Math.min(Math.abs(Math.cos(degreeToRadian(increment / 2)) * 100), 100)}%, 50%)`;
     ctx.stroke();
 }
 
